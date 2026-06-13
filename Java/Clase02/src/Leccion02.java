f20f8a5bdf9ff7cc01ee33c05aa67b6ac2512123
Felipao22 <felipeaviani@gmail.com>
2026-06-13 14:13:09 -0300

    Ingresamos nuestra carpeta Java al commit

---
+++ Java/Clase02/src/Leccion02.java
@@ -0,0 +1,17 @@
+
+public class Leccion02 {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde Java");
+
+        int miVariable = 10;
+
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        //Tipos String
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo en programación";
+        System.out.println(miVariableCadena);
+    }
+}
