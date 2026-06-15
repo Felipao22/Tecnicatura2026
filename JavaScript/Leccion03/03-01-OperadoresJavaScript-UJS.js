ce626159ccbc4a63bcf59875ea0f06d5da23b15a
Felipao22 <felipeaviani@gmail.com>
2026-06-14 22:23:58 -0300

    Agrego el commit de la clase 8 de JavaScript

--- JavaScript/Leccion03/03-01-OperadoresJavaScript-UJS.js
+++ JavaScript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -13,3 +13,50 @@ if (edad >= adulto) {
 } else {
     console.log("Usted es una persona menor de edad")
 }
+
+//Ejercicio: dentro de un rango
+
+let dentroRango = 10; //Aqui vamos a ir cambiando el valor
+let valMin = 0, valMax = 10;
+if (dentroRango >= valMin && dentroRango <= valMax) {
+    console.log("Está dentro del rango establecido")
+} else {
+    console.log("No está dentro del rango establecido")
+
+}
+
+//Ejercicio: si el padre puede asistir al juego de su hijo
+let vacaciones = true, diaDescanso = false;
+if (vacaciones || diaDescanso) {
+    console.log("El padre asistir al juego de su hijo")
+} else {
+    console.log("El padre no puede asistir al juego de su hijo")
+}
+
+//Operador ternario
+let resultado2 = 3 > 2 ? "Verdadero" : "Falso";
+console.log(resultado2);
+let numero = 12;
+resultado2 = numero % 2 === 0 ? "Es un número PAR" : "Es un número IMPAR";
+
+//Convertir String a Number
+let miNumero = "21";//Es una cadena
+console.log(typeof miNumero);
+let edad2 = Number(miNumero)//Esta es una función
+console.log(edad2);
+if (isNaN(edad2)) { //No es número = is Not a Number(devuelve un resultado booleano)
+    console.log("La variable no contiene solo números")
+} else {
+    if (edad2 >= 18) {
+        console.log("Puede votar");
+    } else {
+        console.log("Muy joven para votar");
+    }
+
+}
+
+
+let resultado3 = edad2 >= 18 ? "Puede votar" : "Muy joven para votar"
+console.log(resultado3)
+
+//Funcion isNaN
