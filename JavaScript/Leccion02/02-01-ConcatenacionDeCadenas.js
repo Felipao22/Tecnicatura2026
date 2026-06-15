8d5bad2d72c145520123a88a66d37753ecd81f87
Felipao22 <felipeaviani@gmail.com>
2026-06-14 21:57:55 -0300

    Agrego el commit de la clase 7 de JavaScript

--- JavaScript/Leccion02/02-01-ConcatenacionDeCadenas.js
+++ JavaScript/Leccion02/02-01-ConcatenacionDeCadenas.js
@@ -13,3 +13,21 @@ console.log(juntos);
 
 nombre += apellido; //Concatenamos usando el operador simplificado
 console.log(nombre);
+
+// Hoy ya no se usa var, se usa let y const
+let nombre2 = "Pedro";
+console.log(nombre2);
+const apellido2 = " Perez";
+//apellido2 = "Perez"; una constante que no puede ser modificada
+console.log(apellido2);
+
+let x, y; //Se pueden crear varias variables dentro de una misma línea
+x = 17, y = 21; //Se puede hacer asignación de varias variables dentro de la misma línea
+let z = x + y; //Se asigna el valor de la operación
+console.log(z)
+
+let _1num = 31; //No utilizar número para iniciar el nombre de una variable
+let rompiendo = "rompe"; //NO utilizar palabras reservadas para variables
+
+console.log(_1num);
+console.log(rompiendo);
---
+++ JavaScript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,15 @@
+//Ejercicios para encontrar número pares e impares
+let parImpar = 1;
+if (parImpar % 2 == 0) {
+    console.log("Es un número PAR");
+} else {
+    console.log("Es un númnero IMPAR")
+}
+
+//Ejercicio es mayor de edad
+let edad = 18, adulto = 18;
+if (edad >= adulto) {
+    console.log("Es una persona adulta")
+} else {
+    console.log("Usted es una persona menor de edad")
+}
